55ef756cc1dc5297babab99d7b87dfd3a3e9283c
Campi <federico.campi@bizland.tech>
2025-01-25 20:37:05 -0300

    SEO y google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
         </script>
         {/* <!-- Google tag (gtag.js) --> */}
         <Script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
-        <Script>
+        <Script id="ga-script" strategy="afterInteractive">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
